92ef8f1062ced9ad7f969de6ab2f60b81ee6d347
Val-entine92 <111467473+Val-entine92@users.noreply.github.com>
2022-10-29 02:47:27 -0400

    commit

--- helloworld.js
+++ helloworld.js
@@ -1,8 +1,11 @@
-const $ = document.querySelector.bind(document);
-
+const g = document.querySelector.bind(document);
 function hello(){
-    userResponse=prompt('Enter your name:');
-    if (userResponse);
-    }
-    
+        alert('Hello')}
+function openwindow(){
+            var newwindow=window.open(",", 'width=400, height=400');
+            newwindow.document.write("<button onclick='window.close()'>Back</button>")}
+               
+
+document.body.innerHTML+='<b></b>'  
+g ('#par').innerHTML='My name is Prince'  
     
