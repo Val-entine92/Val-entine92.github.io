d2ae6fc1e1e6af267ce9600a1d7e30389749494a
Val-entine92 <111467473+Val-entine92@users.noreply.github.com>
2022-12-11 07:26:00 -0500

    Update weatherapp.js

--- weatherapp.js
+++ weatherapp.js
@@ -10,33 +10,13 @@ const backButton=container.querySelector(".back");
 setInterval(//runs the function at at interval of 1 second
 function getCurrentTime(){//function to obtain time ,day, date info and put into HTMl
 let currentTimeDate = new Date();
-var weekday = new Array(7);
-        weekday[0] = "Mon";
-        weekday[1] = "Tue";
-        weekday[2] = "Wed";
-        weekday[3] = "Thu";
-        weekday[4] = "Fri";
-        weekday[5] = "Sat";
-        weekday[6] = "Sun"
-var month = new Array();
-        month[0] = "Jan";
-        month[1] = "Feb";
-        month[2] = "Mar";
-        month[3] = "Apr";
-        month[4] = "May";
-        month[5] = "Jun";
-        month[6] = "Jul";
-        month[7] = "Aug";
-        month[8] = "Sep";
-        month[9] = "Oct";
-        month[10] = "Nov";
-        month[11] = "Dec";
-    
-let currentDay = weekday[currentTimeDate.getDay()];
+var day= ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+var month = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+let currentDay = day[currentTimeDate.getDay()];
 let currentDate  = currentTimeDate.getDate();
 let currentMonth = month[currentTimeDate.getMonth()];
 let currentYear = currentTimeDate.getFullYear();
-let fullDate = ` ${currentDay} + ${currentDate} + ${currentMonth}, ${CurrentYear}`;
+let fullDate = ` ${currentDay} ${currentDate} ${currentMonth}, ${currentYear}`;
 $(".time").innerHTML =`[${fullDate} ] `  + currentTimeDate.toLocaleTimeString();
 });
 
@@ -45,12 +25,11 @@ let api_url;
 window.addEventListener("load",()=>{//upon loading , function will run to provide current temperature of base city (Caldwell)
     api_url=(`https://api.openweathermap.org/data/2.5/weather?q=Caldwell&units=metric&appid=0461ab343c61a04b435c6ed2fc6d01b9`);
     fetch(api_url).
-    then(res => res.json())
-   .then(wData=> (weatherDet(wData)));
+    then(res => res.json()) .then(wData=> (weatherDet(wData)));
     function weatherDet(wData){
         if (wData.cod==200){
         let temp = wData.main.temp*(9/5) + 32;
-        $(".temperature").innerHTML = Math.floor(temp);
+        $(".temperature").innerHTML = Math.ceil(temp);
            }}
 })
 
@@ -90,54 +69,53 @@ function fetchInfo(){//function to gain API info
     fetch(api_url).
     then(res => res.json())
     .then(wInfo=> (weatherDetails(wInfo)))//Api info converted to JSON is given the object ,wInfo
-}
+        function weatherDetails(wInfo){//putting thenobtained info into the HTML 
+            console.log(wInfo)
+            if (wInfo.cod===200){
+                container.classList.add("active");
+                let city = wInfo.name;
+                let country = wInfo.sys.country;
+                let temp = wInfo.main.temp*(9/5) + 32;
+                let description= wInfo.weather[0].description;
+                let feels_like = wInfo.main.feels_like*(9/5 ) + 32;
+                let humidity = wInfo.main.humidity;
+                $(".number").innerHTML = Math.floor(temp);
+                $(".weather").innerHTML = description;
+                $(".location").innerHTML = `${city}, ${country}`;
+                $(".feels").innerHTML = Math.floor(feels_like);
+                $(".humidity").innerHTML = `${humidity}%`;
 
-
-function weatherDetails(wInfo){//putting thenobtained info into the HTML 
-    console.log(wInfo)
-    if (wInfo.cod==200){
-        container.classList.add("active");
-        let city = wInfo.name;
-        let country = wInfo.sys.country;
-        let temp = wInfo.main.temp*(9/5) + 32;
-        let description= wInfo.weather[0].description;
-        let feels_like = wInfo.main.feels_like*(9/5) + 32;;
-        let humidity = wInfo.main.humidity;
-        $(".number").innerHTML = Math.floor(temp);
-        $(".weather").innerHTML = description;
-        $(".location").innerHTML = `${city}, ${country}`;
-        $(".feels").innerHTML = Math.floor(feels_like);
-        $(".humidity").innerHTML = `${humidity}%`;
-
-        let id = wInfo.weather[0].id;
-        function changeImage(url){//style backgrounds 
-            backgroundImage.style.background="url('"+url+"')";
-            backgroundImage.style.backgroundRepeat="no-repeat";
-            backgroundImage.style.backgroundSize="cover";
-            backgroundImage.style.backgroundColor="#9dc2db"
-        }
-        //each set of ids is accompanied by a different background image 
-        if (id>=200&&id<=232){//thunderstorm 
-            changeImage('https://th.bing.com/th/id/R.25a81d3a9fd98b4bc763917ce6a6979a?rik=s5jis36eYsQpIw&pid=ImgRaw&r=0');
-        }else if (id>=300&&id<=321){//drizzle 
-            changeImage('https://th.bing.com/th/id/R.6c9680a5a515d2b459e44911babdb3cd?rik=%2bbMmOXw%2f%2fZl8tQ&pid=ImgRaw&r=0');
-        }else if (id>=500&&id<=531){//rain
-            changeImage('https://th.bing.com/th/id/R.258e88c6cb21ea8099f08ec2ce2cf999?rik=%2fCfwqAwXw7rkzg&riu=http%3a%2f%2fgetwallpapers.com%2fwallpaper%2ffull%2fb%2fa%2f1%2f944162-raining-wallpaper-1920x1080-for-mac.jpg&ehk=5qd%2fAlfsY325l%2fRwlBZ7ro5%2fkM%2f92%2bnZL%2fIBcSzduXY%3d&risl=&pid=ImgRaw&r=0')
-        }else if (id>=600&&id<=622){//snow
-            changeImage('https://th.bing.com/th/id/R.48e6824e5ff37b0ba8ad52e2b40ae3ab?rik=oQqQ30mlhDP61g&pid=ImgRaw&r=0');
-        }else if (id>=700&&id<=781){//atmosphere.ie.mist, smoke, haze, winds, etc
-            changeImage('https://th.bing.com/th/id/OIP.y_499sPqaKtg9NLAS_T_MwHaE8?pid=ImgDet&rs=1');
-        }if (id==800){//clear sky
-            changeImage('https://th.bing.com/th/id/R.c5e078e3c50284dd2b23396a4749bed7?rik=rwcTZRM3npqLww&pid=ImgRaw&r=0');
-        }if (id>=801&&id<=804){//clouds
-            changeImage('https://i.pinimg.com/originals/fc/fd/b7/fcfdb71f4bb464b00fc7e5fd3b51be6a.jpg');
+                let id = wInfo.weather[0].id;
+                function changeImage(url){//style backgrounds 
+                    backgroundImage.style.background="url('"+url+"')";
+                    backgroundImage.style.backgroundRepeat="no-repeat";
+                    backgroundImage.style.backgroundSize="cover";
+                    backgroundImage.style.backgroundColor="inherit";  
+                }
+                //each set of ids is accompanied by a different background image 
+                if (id>=200&&id<=232){//thunderstorm 
+                    changeImage('https://th.bing.com/th/id/R.25a81d3a9fd98b4bc763917ce6a6979a?rik=s5jis36eYsQpIw&pid=ImgRaw&r=0');
+                }else if (id>=300&&id<=321){//drizzle 
+                    changeImage('https://th.bing.com/th/id/R.6c9680a5a515d2b459e44911babdb3cd?rik=%2bbMmOXw%2f%2fZl8tQ&pid=ImgRaw&r=0');
+                }if (id>=500&&id<=531){//rain
+                    changeImage('https://th.bing.com/th/id/R.258e88c6cb21ea8099f08ec2ce2cf999?rik=%2fCfwqAwXw7rkzg&riu=http%3a%2f%2fgetwallpapers.com%2fwallpaper%2ffull%2fb%2fa%2f1%2f944162-raining-wallpaper-1920x1080-for-mac.jpg&ehk=5qd%2fAlfsY325l%2fRwlBZ7ro5%2fkM%2f92%2bnZL%2fIBcSzduXY%3d&risl=&pid=ImgRaw&r=0')
+                }else if (id>=600&&id<=622){//snow
+                    changeImage('https://th.bing.com/th/id/R.48e6824e5ff37b0ba8ad52e2b40ae3ab?rik=oQqQ30mlhDP61g&pid=ImgRaw&r=0');
+                }if (id>=700&&id<=781){//atmosphere.ie.mist, smoke, haze, winds, etc
+                    changeImage(' https://th.bing.com/th/id/R.d8a466b8f779ddbb17b0852c20b616dd?rik=oAep%2fAWOahOC8g&pid=ImgRaw&r=0');
+                $(".weather-section").style.color="white";
+                }else if (id==800){//clear sky
+                    changeImage('https://th.bing.com/th/id/R.c5e078e3c50284dd2b23396a4749bed7?rik=rwcTZRM3npqLww&pid=ImgRaw&r=0');
+                }if (id>=801&&id<=804){//clouds
+                    changeImage('https://i.pinimg.com/originals/fc/fd/b7/fcfdb71f4bb464b00fc7e5fd3b51be6a.jpg');
+                }
+            }
+            else if (wInfo.cod==404){//code for an invalid input
+                infoText.classList.add("error");
+                infoText.innerHTML = `${inputField.value} isn't a valid location`;
+            }
         }
     }
-    else if (wInfo.cod==404){//code for an invalid input
-        infoText.classList.add("error");
-        infoText.innerHTML = `${inputField.value} isn't a valid city name`;
-    }
-}
 backButton.addEventListener("click",()=>{
         container.classList.remove("active" );
         infoText.classList.remove("error");
